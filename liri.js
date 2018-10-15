c8c3b6857ed49ee050fe9e97919751ed82ec3acc
mjbenefiel <michaeljbenefiel@gmail.com>
2018-10-15 18:19:56 -0500

    10152018 commits initial setup

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 
+var fs = require('fs');
+var request = require("request");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+
+
 var spotify = new Spotify(keys.spotify);
+
+
